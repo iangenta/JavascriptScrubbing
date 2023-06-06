be3884c8f842a6c30d080187e593ca372d75fd9a
iangenta <64538146+iangenta@users.noreply.github.com>
2023-06-06 01:25:32 -0300

    iteration created to take values from active timelines

--- script.js
+++ script.js
@@ -1,78 +1,109 @@
-var timeline = document.getElementById('timeline');
-var scrubber = document.getElementById('scrubber');
-var mediaContainer = document.getElementById('media-container');
-var mediaItems = Array.from(document.querySelectorAll('.media'));
-var totalDuration = mediaItems.reduce((total, item) => total + parseFloat(item.getAttribute('data-duration')), 0);
-var dragging = false;
-var activeVideo = null;
+var timelines = Array.from(document.querySelectorAll('.timeline'));
+var draggings = [];
+var activeVideos = [];
 
-// Function to get the position on the timeline based on event coordinates
+timelines.forEach(function(timeline, index) {
+  var scrubber = timeline.querySelector('.scrubber');
+  var mediaContainer = timeline.querySelector('.media-container');
+  var mediaItems = Array.from(mediaContainer.querySelectorAll('.media'));
+  var totalDuration = mediaItems.reduce((total, item) => total + parseFloat(item.getAttribute('data-duration')), 0);
 
-function getPosition(event) {
-  var timelineRect = timeline.getBoundingClientRect();
-  var clientX = event.clientX || event.touches[0].clientX;
-  return parseFloat(((clientX - timelineRect.left) / timelineRect.width).toFixed(2)); // Rounded to two decimals
-}
+  function getPosition(event) {
+    var timelineRect = timeline.getBoundingClientRect();
+    var clientX = event.clientX || event.touches[0].clientX;
+    return parseFloat(((clientX - timelineRect.left) / timelineRect.width).toFixed(2)); // Rounded to two decimals
+  }
 
-// Update the timeline position and active media based on mouse movement and touch events
+  function handleMoveEvent(event) {
+    event.preventDefault();
+    if (draggings[index]) {
+      var position = getPosition(event);
+      var currentTime = position * totalDuration;
+      updateScrubberPosition(scrubber, position);
+      updateMedia(mediaItems, currentTime, index);
+    }
+  }
 
-function handleMoveEvent(event) {
-  event.preventDefault();
-  if (!dragging) {
+  function handleStartEvent(event) {
+    draggings[index] = true;
+    timeline.classList.add('dragging');
+
     var position = getPosition(event);
     var currentTime = position * totalDuration;
 
-    updateScrubberPosition(position);
-    updateMedia(currentTime);
+    updateScrubberPosition(scrubber, position);
+    updateMedia(mediaItems, currentTime, index);
+
+    // Update the active media based on the current time
+    var cumulativeTime = 0;
+    var activeMedia = null;
+
+    mediaItems.forEach(function(item) {
+      var duration = parseFloat(item.getAttribute('data-duration'));
+      cumulativeTime += duration;
+
+      if (!activeMedia && currentTime < cumulativeTime) {
+        activeMedia = item;
+      }
+    });
+
+    mediaItems.forEach(function(item) {
+      if (item === activeMedia) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+
+    // Play the video from the current time
+    var mediaType = activeMedia.getAttribute('data-type');
+    var mediaContent = activeMedia.querySelector('video') || activeMedia.querySelector('img');
+
+    if (mediaType === 'video' && mediaContent.paused) {
+      var video = mediaContent;
+      video.currentTime = currentTime - (cumulativeTime - duration);
+      if (activeVideos[index] && activeVideos[index] !== video) {
+        pauseActiveVideo(index);
+      }
+      activeVideos[index] = video;
+      video.play();
+    }
   }
-}
 
-function handleStartEvent(event) {
-  dragging = true;
-  timeline.classList.add('dragging');
-
-  var position = getPosition(event);
-  var currentTime = position * totalDuration;
-
-  updateScrubberPosition(position);
-  updateMedia(currentTime);
-}
-
-function handleEndEvent() {
-  if (dragging) {
-    dragging = false;
-    timeline.classList.remove('dragging');
+  function handleEndEvent() {
+    if (draggings[index]) {
+      draggings[index] = false;
+      timeline.classList.remove('dragging');
+      pauseActiveVideo(index);
+    }
   }
-}
 
-function handleTouchMove(event) {
-  event.preventDefault();
-  if (dragging) {
-    var position = getPosition(event);
-    var currentTime = position * totalDuration;
+  function handleTouchMove(event) {
+    event.preventDefault();
+    if (draggings[index]) {
+      var position = getPosition(event);
+      var currentTime = position * totalDuration;
 
-    updateScrubberPosition(position);
-    updateMedia(currentTime);
+      updateScrubberPosition(scrubber, position);
+      updateMedia(mediaItems, currentTime, index);
+    }
   }
-}
 
-timeline.addEventListener('mousemove', handleMoveEvent);
-timeline.addEventListener('mousedown', handleStartEvent);
-window.addEventListener('mouseup', handleEndEvent);
+  timeline.addEventListener('mousemove', handleMoveEvent);
+  timeline.addEventListener('mousedown', handleStartEvent);
+  mediaContainer.addEventListener('mouseenter', handleStartEvent);
+  window.addEventListener('mouseup', handleEndEvent);
 
-timeline.addEventListener('touchmove', handleTouchMove, { passive: false });
-timeline.addEventListener('touchstart', handleStartEvent, { passive: false });
-window.addEventListener('touchend', handleEndEvent);
+  timeline.addEventListener('touchmove', handleTouchMove, { passive: false });
+  timeline.addEventListener('touchstart', handleStartEvent, { passive: false });
+  window.addEventListener('touchend', handleEndEvent);
+});
 
-// Update the position of the scrubber on the timeline
-
-function updateScrubberPosition(position) {
+function updateScrubberPosition(scrubber, position) {
   scrubber.style.left = (position * 100).toFixed(2) + '%'; // Rounded to two decimals
 }
 
-// Update the active media based on the current time
-
-function updateMedia(currentTime) {
+function updateMedia(mediaItems, currentTime, index) {
   var cumulativeTime = 0;
   var activeMedia = null;
 
@@ -82,14 +113,6 @@ function updateMedia(currentTime) {
 
     if (!activeMedia && currentTime < cumulativeTime) {
       activeMedia = item;
-      var mediaType = item.getAttribute('data-type');
-      var mediaContent = item.querySelector('video') || item.querySelector('img');
-
-      if (mediaType === 'video') {
-        var position = parseFloat(((currentTime - (cumulativeTime - duration)) / duration).toFixed(2)); // Rounded to two decimals
-        mediaContent.currentTime = position * mediaContent.duration;
-        activeVideo = mediaContent;
-      }
     }
   });
 
@@ -100,4 +123,11 @@ function updateMedia(currentTime) {
       item.classList.remove('active');
     }
   });
+}
+
+function pauseActiveVideo(index) {
+  if (activeVideos[index]) {
+    activeVideos[index].pause();
+    activeVideos[index] = null;
+  }
 }
